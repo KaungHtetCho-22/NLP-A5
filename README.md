faf1ba4aa1ed99b8cd1bcb1b91c5776fe90e07ba
KaungHtetCho-22 <kaunghtetcho.ait@gmail.com>
2024-03-02 17:17:07 +0700

    update README

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # NLP-A5
 NLP assignment from AIT
+
+**Kaung Htet Cho (st124092)**
+
+## Table of Contents
+1. [Task1](#Task1)
+2. [Task2](#Task2)
+3. [Task3](#Task3)
+4. [Task4](#Task4)
+
+
+## Task1
+### Train BERT from scratch
+
+- training_data = Harry potter book 1 (https://github.com/bobdeng/owlreader/blob/master/ERead/assets/books/Harry%20Potter%20and%20the%20Order%20of%20the%20Phoenix.txt)
+- vocab_size = 7485
+- total_sentence = 6184
+- encoder_layers = 6
+
+Trained the base BERT model saved the bert_from_scratch.pth for later use in sentence-BERT
+
+## Task2
+### Sentence BERT 
+
+1. Load the datasets (SNLI, MNLI) from hugging face
+2. Load weight file from last trained BERT base model and took the output, done Objective function
+
+## Task3
+### Evaluation and Analysis
+
+1. Evaluation of sentence transformer model: 
+2. Performance comparison
+3. Impact of Hyperparameters: 
+4. Challenges: Personal GPU which can have lower GPU memory can face CUDA out of Memory in inferencing the sentence BERT
+
+## Task4
+### Text-similarity Development
+
+The Website can be accessed on http://localhost:8000. User can type two different sentences and then my trained sentence bert model processed the objective function and shown the cosine similarity between the two sentences
--- app/app.py
+++ app/app.py
@@ -2,27 +2,23 @@
 import os 
 import pickle
 import torch
-
 from utils import *
 from flask import Flask, render_template, request, jsonify, send_file
+device    = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # load the data
-Data         = pickle.load(open('./data/Data.pkl', 'rb'))
-max_len      = Data['max_len']
-max_mask     = Data['max_mask']
-word2id      = Data['word2id']
-
+Data     = pickle.load(open('./data/Data.pkl', 'rb'))
+max_len  = Data['max_len']
+max_mask = Data['max_mask']
+word2id  = Data['word2id']
 
 tokenizer = customTokenizer(word2id)
 
-
-save_path = f'./model/sentenceBERT.pth'
-device    = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # device = 'cpu'
-model     = BERT()
-model.load_state_dict(torch.load(save_path))
+model = BERT()
+model.load_state_dict(torch.load('./model/sentenceBERT.pth'))
 model.to(device)
-# model.eval()
+model.eval()
 
 # flask app
 app = Flask(__name__, static_url_path = '/static')
@@ -33,18 +29,16 @@ def index():
         result = None
         return render_template('index.html', result=result)
     
-    if request.method == 'POST':        
+    if request.method == 'POST':  
+
         sentence_a = request.form.get('sen1')
         sentence_b = request.form.get('sen2')
         print(sentence_a, sentence_b)
+
         result = calculate_similarity(model, tokenizer, sentence_a, sentence_b, device)
         return render_template('index.html', result=result, sen1 = sentence_a, sen2 = sentence_b)
-
-
 
 port_number = 8000
 
 if __name__ == '__main__':
     app.run(debug = True, host='0.0.0.0', port=port_number)
-
-
